a2d9a5ef25336cfe81283897f5eff366ce2cc17e
haeseong <hesung0805@gmail.com>
2022-11-22 09:51:41 +0900

    Initial Commit

---
+++ TCA_Test/TCA_TestApp.swift
@@ -0,0 +1,17 @@
+//
+//  TCA_TestApp.swift
+//  TCA_Test
+//
+//  Created by markcloud on 2022/11/22.
+//
+
+import SwiftUI
+
+@main
+struct TCA_TestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
